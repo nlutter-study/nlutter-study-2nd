539fa8f1134dfc6ea8f39bd0aaeae91ad8790e0d
wonjangcloud9 <venuscwj@naver.com>
2023-11-07 23:49:52 +0900

    comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -34,14 +35,81 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size16,
+            vertical: Sizes.size10,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
           itemCount: 5,
           itemBuilder: (context, index) {
-            return ListTile(
-              leading: const CircleAvatar(),
-              title: Text("Comment $index"),
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.grey,
+                ),
+                Gaps.h10,
+                const Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "원장",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size14,
+                      ),
+                    ),
+                    Gaps.v3,
+                    Text(
+                      "여기는 우리 집 안이야",
+                      style: TextStyle(
+                        fontSize: Sizes.size14,
+                      ),
+                    ),
+                  ],
+                )),
+                Gaps.h10,
+                Column(
+                  children: [
+                    Icon(
+                      FontAwesomeIcons.heart,
+                      size: Sizes.size20,
+                      color: Colors.grey.shade500,
+                    ),
+                    Gaps.v2,
+                    Text(
+                      "1.2k",
+                      style: TextStyle(
+                        fontSize: Sizes.size14,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                )
+              ],
             );
           },
+        ),
+        bottomNavigationBar: const BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                child: Text("원장"),
+              ),
+              Gaps.h10,
+              Expanded(
+                  child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Add a comment...",
+                ),
+              )),
+            ],
+          ),
         ),
       ),
     );
