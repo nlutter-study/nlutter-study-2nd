18958a05d4e2a6c133f7f0f703e5742784fe06e1
wonjangcloud9 <venuscwj@naver.com>
2023-11-09 19:22:21 +0900

    text input actions

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -11,8 +11,23 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  bool _isWriting = false;
+
   void _onClosePressed() {
     Navigator.of(context).pop();
+  }
+
+  void _stopWriting() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  void _onStartWriting() {
+    setState(() {
+      _isWriting = true;
+    });
   }
 
   @override
@@ -37,116 +52,157 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: Stack(
-          children: [
-            ListView.separated(
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size16,
-                vertical: Sizes.size10,
-              ),
-              separatorBuilder: (context, index) => Gaps.v20,
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CircleAvatar(
-                      radius: 18,
-                      backgroundColor: Colors.grey,
-                    ),
-                    Gaps.h10,
-                    const Expanded(
-                        child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "원장",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: Sizes.size14,
-                          ),
-                        ),
-                        Gaps.v3,
-                        Text(
-                          "여기는 우리 집 안이야",
-                          style: TextStyle(
-                            fontSize: Sizes.size14,
-                          ),
-                        ),
-                      ],
-                    )),
-                    Gaps.h10,
-                    Column(
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size20,
-                          color: Colors.grey.shade500,
-                        ),
-                        Gaps.v2,
-                        Text(
-                          "1.2k",
-                          style: TextStyle(
-                            fontSize: Sizes.size14,
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                );
-              },
-            ),
-            Positioned(
-              width: size.width,
-              bottom: 0,
-              child: BottomAppBar(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: Sizes.size16,
-                    vertical: Sizes.size10,
-                  ),
-                  child: Row(
+        body: GestureDetector(
+          onTap: _stopWriting,
+          child: Stack(
+            children: [
+              ListView.separated(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.size16,
+                  vertical: Sizes.size10,
+                ),
+                separatorBuilder: (context, index) => Gaps.v20,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const CircleAvatar(
                         radius: 18,
-                        child: Text("원장"),
+                        backgroundColor: Colors.grey,
                       ),
                       Gaps.h10,
-                      Expanded(
-                          child: SizedBox(
-                        height: Sizes.size44,
-                        child: TextField(
-                          expands: true,
-                          maxLines: null,
-                          minLines: null,
-                          textInputAction: TextInputAction.newline,
-                          cursorColor: Theme.of(context).primaryColor,
-                          decoration: InputDecoration(
-                            border: const OutlineInputBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(
-                                  Sizes.size12,
-                                ),
-                              ),
-                              borderSide: BorderSide.none,
+                      const Expanded(
+                          child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "원장",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size14,
                             ),
-                            filled: true,
-                            fillColor: Colors.grey.shade200,
-                            contentPadding: const EdgeInsets.symmetric(
-                              horizontal: Sizes.size12,
-                            ),
-                            hintText: "댓글을 입력하세요",
                           ),
-                        ),
+                          Gaps.v3,
+                          Text(
+                            "여기는 우리 집 안이야",
+                            style: TextStyle(
+                              fontSize: Sizes.size14,
+                            ),
+                          ),
+                        ],
                       )),
+                      Gaps.h10,
+                      Column(
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size20,
+                            color: Colors.grey.shade500,
+                          ),
+                          Gaps.v2,
+                          Text(
+                            "1.2k",
+                            style: TextStyle(
+                              fontSize: Sizes.size14,
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        ],
+                      )
                     ],
+                  );
+                },
+              ),
+              Positioned(
+                width: size.width,
+                bottom: 0,
+                child: BottomAppBar(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size16,
+                      vertical: Sizes.size10,
+                    ),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 18,
+                          child: Text("원장"),
+                        ),
+                        Gaps.h10,
+                        Expanded(
+                            child: SizedBox(
+                          height: Sizes.size44,
+                          child: TextField(
+                            onTap: _onStartWriting,
+                            expands: true,
+                            maxLines: null,
+                            minLines: null,
+                            textInputAction: TextInputAction.newline,
+                            cursorColor: Theme.of(context).primaryColor,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    Sizes.size12,
+                                  ),
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey.shade200,
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: Sizes.size12,
+                              ),
+                              hintText: "댓글을 입력하세요",
+                              suffixIcon: Padding(
+                                  padding: const EdgeInsets.only(
+                                    right: Sizes.size14,
+                                  ),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Icon(
+                                        FontAwesomeIcons.at,
+                                        size: Sizes.size20,
+                                        color: Colors.grey.shade600,
+                                      ),
+                                      Gaps.h14,
+                                      Icon(
+                                        FontAwesomeIcons.gift,
+                                        size: Sizes.size20,
+                                        color: Colors.grey.shade600,
+                                      ),
+                                      Gaps.h14,
+                                      Icon(
+                                        FontAwesomeIcons.faceSmile,
+                                        size: Sizes.size20,
+                                        color: Colors.grey.shade600,
+                                      ),
+                                      Gaps.h14,
+                                      if (_isWriting)
+                                        GestureDetector(
+                                          onTap: _stopWriting,
+                                          child: FaIcon(
+                                            FontAwesomeIcons.circleArrowUp,
+                                            size: Sizes.size20,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                          ),
+                                        )
+                                    ],
+                                  )),
+                            ),
+                          ),
+                        )),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
