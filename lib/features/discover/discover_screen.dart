d75767b8fd9f8ed596547eca9b7a1e5688abc39b
wonjangcloud9 <venuscwj@naver.com>
2023-11-10 21:22:39 +0900

    GirdView

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -44,7 +44,27 @@ class DiscoverScreen extends StatelessWidget {
           ),
         ),
         body: TabBarView(children: [
-          for (var tab in tabs)
+          GridView.builder(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size6,
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: Sizes.size10,
+              mainAxisSpacing: Sizes.size10,
+              childAspectRatio: 9 / 16,
+            ),
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return Container(
+                color: Colors.grey.shade300,
+                child: Center(
+                  child: Text("$index"),
+                ),
+              );
+            },
+          ),
+          for (var tab in tabs.skip(1))
             Center(
               child: Text(tab),
             ),
