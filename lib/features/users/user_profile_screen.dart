1dafa6762fb3b5ed712f1e1870a50d20638d4451
wonjangcloud9 <venuscwj@naver.com>
2023-12-12 00:32:21 +0900

    프로필쪽 완성

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -9,95 +11,244 @@ class UserProfileScreen extends StatefulWidget {
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
+  final faker = Faker();
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          pinned: true,
-          backgroundColor: Colors.teal,
-          collapsedHeight: 80,
-          expandedHeight: 200,
-          flexibleSpace: FlexibleSpaceBar(
-            stretchModes: const [
-              StretchMode.blurBackground,
-              StretchMode.zoomBackground,
-            ],
-            background: Image.asset(
-              "assets/images/moi.jpeg",
-              fit: BoxFit.cover,
+          leading: IconButton(
+            icon: FaIcon(
+              FontAwesomeIcons.globe,
+              color: Colors.black.withOpacity(0.8),
             ),
-            title: const Text("Hello!"),
+            onPressed: null,
           ),
-        ),
-        SliverFixedExtentList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.red[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text("Item $index"),
+          actions: [
+            IconButton(
+              icon: const FaIcon(
+                FontAwesomeIcons.instagram,
               ),
+              onPressed: () {},
             ),
-          ),
-          itemExtent: 100,
-        ),
-        SliverPersistentHeader(
-          delegate: CustomDelegate(),
-          floating: true,
-        ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.blue[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text("Item $index"),
+            IconButton(
+              icon: const FaIcon(
+                FontAwesomeIcons.gripLines,
               ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+              vertical: Sizes.size16,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Jane',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: Sizes.size24,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: Sizes.size10,
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'jane_mobbin',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: Sizes.size20,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: Sizes.size6,
+                                    ),
+                                    Container(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: Sizes.size12,
+                                        vertical: Sizes.size8,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: Colors.grey[300],
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      child: Text(
+                                        "threads.net",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: Sizes.size12,
+                                          color: Colors.grey[500],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: Sizes.size72,
+                            ),
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundImage: NetworkImage(
+                                faker.image.image(),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: Sizes.size10,
+                        ),
+                        const Text(
+                          'Plant enthusiast!',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: Sizes.size20,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: Sizes.size20,
+                        ),
+                        Row(
+                          children: [
+                            Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                Positioned(
+                                  child: CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage: NetworkImage(
+                                      faker.image.image(
+                                        random: true,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  left: 20,
+                                  child: CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage: NetworkImage(
+                                      faker.image.image(
+                                        random: true,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: Sizes.size32,
+                            ),
+                            const Text(
+                              '2 followers',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: Sizes.size20,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: Sizes.size20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AnimatedContainer(
+                      width: 190,
+                      duration: const Duration(
+                        milliseconds: 300,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size16,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size12,
+                        ),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+                      child: const AnimatedDefaultTextStyle(
+                        duration: Duration(
+                          milliseconds: 300,
+                        ),
+                        style: TextStyle(
+                          fontSize: Sizes.size16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                        child: Text(
+                          "Edit Profile",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    AnimatedContainer(
+                      width: 190,
+                      duration: const Duration(
+                        milliseconds: 300,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size16,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size12,
+                        ),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+                      child: const AnimatedDefaultTextStyle(
+                        duration: Duration(
+                          milliseconds: 300,
+                        ),
+                        style: TextStyle(
+                          fontSize: Sizes.size16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                        child: Text(
+                          "Share profile",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100,
-            mainAxisSpacing: Sizes.size20,
-            crossAxisSpacing: Sizes.size20,
-            childAspectRatio: 1,
-          ),
-        ),
+        )
       ],
     );
-  }
-}
-
-class CustomDelegate extends SliverPersistentHeaderDelegate {
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      color: Colors.indigo,
-      child: const FractionallySizedBox(
-        heightFactor: 1,
-        child: Center(
-          child: Text(
-            'Title!!!!!',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  double get maxExtent => 150;
-
-  @override
-  double get minExtent => 80;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
   }
 }
