10f294b4f5669a6385f51f206e3bea4d77ad69a8
henry <devgony@gmail.com>
2023-12-15 15:26:07 +0900

    fix: restore breaks

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( blabla
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
