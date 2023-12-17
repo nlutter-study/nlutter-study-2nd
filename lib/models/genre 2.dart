0b67e78c3eaed575131acd1c671466d134bb60df
Raymond <akdfhr2@gmail.com>
2023-12-17 22:06:42 +0900

    fix : delete duplicated files

--- lib/models/genre 2.dart
+++
@@ -1,5 +0,0 @@
-class Genre {
-  int id;
-  String name;
-  Genre({required this.id, required this.name});
-}
--- lib/models/movie_model 2.dart
+++
@@ -1,8 +0,0 @@
-class MovieModel {
-  final String title, posterPath;
-  final int id;
-  MovieModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        title = json['title'],
-        posterPath = json['poster_path'];
-}
