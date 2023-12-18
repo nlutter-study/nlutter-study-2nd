a9a3c8b599edec61faaf875bb24bf9c6af22d513
DaehunGwak <rhkrgns56@naver.com>
2023-12-18 23:48:17 +0900

    fix: api 관련 예외처리 로직 수정
    https://github.com/nlutter-study/nlutter-study-2nd/pull/5#discussion_r1426718488

--- movieflix/lib/services/api_service.dart
+++ movieflix/lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 import '../models/movie_meta.dart';
 
@@ -18,9 +19,9 @@ class ApiService {
 
   static Future<List<SimpleMovie>> getPopularMovies() async {
     final uri = Uri.parse('$_baseUrl/$_popularPath');
-    final response = await http.get(uri);
-    _handleErrorStatusCode(response);
-    _printDebug(response.body);
+    final response = await _doRequest(() => http.get(uri));
+    // final response =
+    //     await _doRequest(() => throw ClientException('something error'));
     final List<dynamic> results =
         _utf8JsonDecode(response.bodyBytes)[_resultsField];
     return results.map((e) => SimpleMovie.fromJson(e)).toList();
@@ -28,9 +29,7 @@ class ApiService {
 
   static Future<List<SimpleMovie>> getNowPlayingMovies() async {
     final uri = Uri.parse('$_baseUrl/$_nowPlaying');
-    final response = await http.get(uri);
-    _handleErrorStatusCode(response);
-    _printDebug(response.body);
+    final response = await _doRequest(() => http.get(uri));
     final List<dynamic> results =
         _utf8JsonDecode(response.bodyBytes)[_resultsField];
     return results.map((e) => SimpleMovie.fromJson(e)).toList();
@@ -38,9 +37,7 @@ class ApiService {
 
   static Future<List<SimpleMovie>> getComingSoonMovies() async {
     final uri = Uri.parse('$_baseUrl/$_comingSoon');
-    final response = await http.get(uri);
-    _handleErrorStatusCode(response);
-    _printDebug(response.body);
+    final response = await _doRequest(() => http.get(uri));
     final List<dynamic> results =
         _utf8JsonDecode(response.bodyBytes)[_resultsField];
     return results.map((e) => SimpleMovie.fromJson(e)).toList();
@@ -48,23 +45,20 @@ class ApiService {
 
   static Future<DetailMovie> getDetailMovie(int id) async {
     final uri = Uri.parse('$_baseUrl/$_detail$id');
-    final response = await http.get(uri);
-    _handleErrorStatusCode(response);
-    _printDebug(response.body);
+    final response = await _doRequest(() => http.get(uri));
     return DetailMovie.fromJson(_utf8JsonDecode(response.bodyBytes));
   }
 
-  static void _handleErrorStatusCode(http.Response response) {
-    if (response.statusCode != 200) {
-      throw Exception(
-        'API 요청 실패 (url: ${response.request?.url}, status code: ${response.statusCode}',
-      );
-    }
-  }
-
-  static void _printDebug(dynamic something) {
-    if (kDebugMode) {
-      print(something);
+  static Future<Response> _doRequest(Future<Response> Function() func) async {
+    try {
+      return await func();
+    } on ClientException catch (e) {
+      debugPrint("catch client exception: $e");
+      rethrow; // TODO: 커스텀 익셉션 필요 시 던지기 or 응답 Default 로 변경
+    } catch (e, s) {
+      debugPrint("catch other exceptions: $e");
+      debugPrintStack(stackTrace: s);
+      rethrow;
     }
   }
 
